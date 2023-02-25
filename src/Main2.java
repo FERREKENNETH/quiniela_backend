f05a73b9107a08a5b6e2d2250e9cd83041823cc5
kennethdev88 <kennethdev88@gmail.com>
2023-02-25 18:28:12 +0100

    acabado comprobar columnas iguales

--- src/Main2.java
+++ src/Main2.java
@@ -1,86 +1,75 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main2 {
     static String[] resultadoReal = new String[]{ "1","X","2","1","","X","","","","2","","","2","" };
 
     static List<List<String>> quiniela = new ArrayList<>();
 
+    static Map<Integer, List<Integer>> coincidencias = new HashMap<>();
 
+    static void inicializarCoincidencias() {
+        int idx = 0;
+        for (List<String> ignored : quiniela) {
+            coincidencias.put(idx, new ArrayList<>());
+            idx++;
+        }
+    }
 
-    static int totalIguales = 0;
     public static void main(String[] args) {
 
-        //addApuestas();
-        quiniela.add(new ArrayList<String>() {
-            { add("2");add("2");add("X"); }
-        });
-        quiniela.add(new ArrayList<String>() {
-            { add("2");add("X");add("1"); }
-        });
-        quiniela.add(new ArrayList<String>() {
-            { add("1");add("2");add("X"); }
-        });
+        addApuestas();
+
+        inicializarCoincidencias();
+
+        int colTestIndex = 1;
+        int colTestIndexInterno = 1;
+        int colActualIndex = 0;
+
+        List<String>  colTest;
 
 
-        List<List<String>> columnasIguales = new ArrayList<>();
-        List<List<String>> columnasDiferentes = new ArrayList<>();
-        int a = 0;
-        int b = 0;
-        loop1:
-        for (List<String> col: quiniela) {
-            System.out.println("col modelo " + col);
+        for (List<String> colActual: quiniela) {
+            for (colTestIndexInterno = colTestIndex; colTestIndexInterno < quiniela.size(); colTestIndexInterno++) {
+                colTest = quiniela.get(colTestIndexInterno);
 
+                boolean sonIguales = sonColumnasIguales(colActual, colTest, "");
 
-            loop2:
-            for (List<String> colA: quiniela) {
-                System.out.println("col test   " + quiniela.get(b));
+                if (sonIguales) {
+                    //COMPROBAMOS SI YA EXISTEN CONINCIDENCIAS
+                    boolean existeConicidencia = false;
+                    for (Map.Entry<Integer,List<Integer>> entry : coincidencias.entrySet()) {
+                        List<Integer> repetidos = entry.getValue();
 
-                System.out.println("a = " + a + " b = " + b);
+                        existeConicidencia = repetidos.contains(colTestIndexInterno);
+                        if (existeConicidencia) {
+                            break;
+                        }
 
-                if (col.equals(colA) && a!=b) {
-                    columnasIguales.add(colA);
-                    comparar2Columnas(col, colA, "IGUALES");
-                    totalIguales++;
-                } else if (!col.equals(colA) && a!=b) {
-                    columnasDiferentes.add(colA);
-                    comparar2Columnas(col, colA, "DIFERENTES");
+                        //System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
 
+                    }
+
+                    if (!existeConicidencia)
+                        coincidencias.get(colActualIndex).add(colTestIndexInterno);
                 }
-                b++;
-                //PRINT COLUMNAS REPETIDAS
-                //System.out.println("----------------- colsRepes = " + (columnasIguales.size()-1));
-
             }
 
-
-            System.out.println("---------------------END---------------------");
-
-            //RESET VARIABLES
-            b = 0;
-            a++;
-            columnasIguales = new ArrayList<>();
-            columnasDiferentes = new ArrayList<>();
+            colTestIndex++;
+            colActualIndex++;
+            colTestIndexInterno = colTestIndex;
 
         }
 
-
-        System.out.println("totalIguales = " + totalIguales);
-        System.out.println("+++++++++++++++++++");
+        System.out.println(coincidencias);
+        System.out.println("TRALALA");
 
     }
 
     public static void addApuestas() {
         quiniela.add(new ArrayList<String>() {
-            { add("2");add("2");add("X"); }
-        });
-        quiniela.add(new ArrayList<String>() {
-            { add("2");add("X");add("1"); }
-        });
-        quiniela.add(new ArrayList<String>() {
-            { add("2");add("2");add("X"); }
-        });
-        /*quiniela.add(new ArrayList<String>() {
             { add("2");add("2");add("X");add("X");add("2");add("X");add("2");add("2");add("1");add("1");add("1");add("X");add("1");add("X"); }
         });
         quiniela.add(new ArrayList<String>() {
@@ -104,7 +93,6 @@ public class Main2 {
         quiniela.add(new ArrayList<String>() {
             { add("2");add("X");add("1");add("1");add("X");add("X");add("2");add("2");add("1");add("1");add("X");add("1");add("1");add("2"); }
         });
-
         quiniela.add(new ArrayList<String>() {
             { add("2");add("1");add("X");add("2");add("2");add("2");add("2");add("X");add("1");add("1");add("X");add("1");add("X");add("2"); }
         });
@@ -126,16 +114,15 @@ public class Main2 {
         quiniela.add(new ArrayList<String>() {
             { add("1");add("1");add("X");add("2");add("1");add("2");add("2");add("2");add("1");add("1");add("1");add("X");add("2");add("2"); }
         });
-
         quiniela.add(new ArrayList<String>() {
             { add("X");add("1");add("X");add("2");add("2");add("X");add("2");add("2");add("X");add("1");add("X");add("1");add("1");add("X"); }
-        });*/
+        });
 
     }
 
 
 
-    public static void comparar2Columnas(List<String> col1, List<String> col2, String titulo) {
+    public static boolean sonColumnasIguales(List<String> col1, List<String> col2, String titulo) {
         System.out.println("---------------------------------COLUMNAS " + titulo + "---------------------------------");
         int resultadosIguales = 0;
         int resultadosDiferentes = 0;
@@ -151,6 +138,8 @@ public class Main2 {
         System.out.println("TOTAL REPETIDOS: " + resultadosIguales);
         System.out.println("TOTAL NO REPETIDOS: " + resultadosDiferentes);
         System.out.println("---------------------------------COLUMNAS " + titulo + "---------------------------------\n\n");
+
+        return resultadosDiferentes == 0;
 
     }
 
